452faff760ddaa37e9ff97035df8d207542e5789
Alexandre <alex@toutoleron.com>
2021-09-22 19:13:33 +0200

    - création du brute force ( sans POO, met plus de temps )

--- brute_force.py
+++ brute_force.py
@@ -1,8 +1,50 @@
 from itertools import combinations
 
 
-def brute_force(n, actions, wallet):
-    leafs = []
+def brute_force(names, prices, profits, W):
+    """
+    Brute force algorithm solving knapsack problem
+    :param names: names of the actions used as parameters
+    :param prices: prices of each actions
+    :param profits: profits of each action
+    :param W: our wallet, or maximum capacity of knapsack
+    :return: best actions set, total profits, total cost
+    """
+    n = len(names)
+    best_actions= None
+    best_actions_profits = 0
+    final_cost = 0
     for i in range(n):
-        for j in list(combinations(actions, i+1)):
-            print(j)
+        print(i)
+        for combination in set(combinations(names, i+1)):
+            cost = 0
+            total_profit = 0
+            for element in combination:
+                cost += prices[names.index(element)]
+                total_profit += (prices[names.index(element)]*profits[names.index(element)])
+            if cost <= 500 and total_profit > best_actions_profits:
+                best_actions = combination
+                best_actions_profits = total_profit
+                final_cost = cost
+    return best_actions, best_actions_profits/100, cost
+
+
+#  fonctionnement avec POO
+# def brute_force(actions, W):
+#     n = len(actions)
+#     best_actions= None
+#     best_actions_profits = 0
+#     final_cost = 0
+#     for i in range(n):
+#         print(i)
+#         for combination in list(combinations(actions, i+1)):
+#             cost = 0
+#             total_profit = 0
+#             for element in combination:
+#                 cost += element.price
+#                 total_profit += element.profitability
+#             if cost <= 500 and total_profit > best_actions_profits:
+#                 best_actions = combination
+#                 best_actions_profits = total_profit
+#                 final_cost = cost
+#     return best_actions, best_actions_profits/100, final_cost
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from file_reader import get_action_list
 from brute_force import brute_force
+import time
 
-data = "data/dataset1_Python+P7.csv"
-actions = get_action_list(data)
+start = time.time()
+data = "data/dataset0_Python+P7.csv"
+names, prices, profits = get_action_list(data) #  fonctionnement sans objet Action
+# actions = get_action_list(data) #  fonctionnement avec POO
+W = 500
 
-brute_force(len(actions), actions, 500)
+
+best_actions, total_profits, final_cost = brute_force(names, prices, profits, W) #  fonctionnement sans objet Action
+# best_actions, total_profits, final_cost  = brute_force(actions, W) #  fonctionnement avec POO
+stop = time.time()
+
+resolve_time = stop - start
+
+print(best_actions, total_profits, final_cost, resolve_time)
+
