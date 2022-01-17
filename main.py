bf31fe6409ba591f335bcf3a2024074e85b232d3
AlexandreDuthil <alex@toutoleron.com>
2022-01-17 16:26:34 +0100

    - mise au point de l'algo optimized_POO ( moins rapide que sans la POO )

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 from file_reader import get_action_list, get_action_list_POO
 from brute_force import brute_force, brute_force_POO
-from optimized import optimized
+from optimized import optimized, optimized_POO
 import time
 
-data = "data/dataset0_Python+P7.csv"
+# data = "data/dataset0_Python+P7.csv"
+data = "data/dataset2_Python+P7.csv"
 W = 500
 
-start = time.time()
-names, prices, profits = get_action_list(data)  # fonctionnement sans objet Action
-best_actions, total_profits, final_cost = brute_force(names, prices, profits, W)  # fonctionnement sans objet Action
-stop = time.time()
 
-start_POO = time.time()
-actions = get_action_list_POO(data)  # fonctionnement avec POO
-best_actions_POO, total_profits_POO, final_cost_POO = brute_force_POO(actions, W)  # fonctionnement avec POO
-stop_POO = time.time()
-
-
-
-print(F"Sans POO : \n"
-      F"Meilleures actions : {best_actions}\n"
-      F"Gains totaux : {total_profits}\n"
-      F"Prix final : {final_cost}\n"
-      F"Running time : {(stop-start)}\n")
-
-print(F"Avec POO : \n"
-      F"Meilleures actions : {best_actions_POO}\n"
-      F"Gains totaux : {total_profits_POO}\n"
-      F"Prix final : {final_cost_POO}\n"
-      F"Running time : {(stop_POO-start_POO)}\n")
-
-# print(optimized(actions, W*100)/100)
+# start = time.time()
+# names, prices, profits = get_action_list(data)  # fonctionnement sans objet Action
+# best_actions, total_profits, final_cost = brute_force(names, prices, profits, W)  # fonctionnement sans objet Action
 # stop = time.time()
-# print(stop - start)
+#
+# start_POO = time.time()
+actions = get_action_list_POO(data)  # fonctionnement avec POO
+# best_actions_POO, total_profits_POO, final_cost_POO = brute_force_POO(actions, W)  # fonctionnement avec POO
+# stop_POO = time.time()
 
-#TODO : tester optimized sans la POO, comparer la rapidité
-#TODO : réorganiser les algo POO/pasPOO, ça devient fouilli
+start_optimized = time.time()
+result = optimized(actions, W)
+stop_optimized = time.time()
+print(F"liste des actions : {result}")
+# print(result.value)
+print(F"running time : {stop_optimized-start_optimized}")
+
+
+# print(F"Brute force sans POO : \n"
+#       F"Meilleures actions : {best_actions}\n"
+#       F"Gains totaux : {total_profits}\n"
+#       F"Prix final : {final_cost}\n"
+#       F"Running time : {(stop-start)}\n")
+#
+# print(F"Brute force avec POO : \n"
+#       F"Meilleures actions : {best_actions_POO}\n"
+#       F"Gains totaux : {total_profits_POO}\n"
+#       F"Prix final : {final_cost_POO}\n"
+#       F"Running time : {(stop_POO-start_POO)}\n")
+
--- optimized.py
+++ optimized.py
@@ -1,31 +1,36 @@
+from Node import Node
 
-
-def optimized_POO(actions, W):
-    n = len(actions)
-    mat = [[0 for x in range( W + 1 )] for x in range( n + 1 )]
-
-    for i in range( n + 1 ):
-        for j in range( W + 1 ):
-            if i == 0 or j == 0:
-                mat[i][j] = 0
-            elif actions[i-1].price < j:
-                mat[i][j] = max(actions[i-1].profitability + mat[i-1][j-actions[i-1].price], mat[i-1][j])
-            else:
-                mat[i][j] = mat[i-1][j]
-    return mat[n][W]
-
-
-# TODO : enlever la notion de POO pour pouvoir comparer avec et sans
 def optimized(actions, W):
     n = len(actions)
     mat = [[0 for x in range( W + 1 )] for x in range( n + 1 )]
 
-    for i in range( n + 1 ):
-        for j in range( W + 1 ):
+    for i in range(n + 1):
+        for j in range(W + 1):
             if i == 0 or j == 0:
                 mat[i][j] = 0
             elif actions[i-1].price < j:
                 mat[i][j] = max(actions[i-1].profitability + mat[i-1][j-actions[i-1].price], mat[i-1][j])
             else:
                 mat[i][j] = mat[i-1][j]
+
+    return mat[n][W]
+
+
+def optimized_POO(actions, W):
+    n = len(actions)
+    mat = [[Node() for x in range(W + 1)] for x in range(n + 1)]
+
+    for i in range(n + 1):
+        for j in range(W + 1):
+            if i == 0 or j == 0:
+                pass
+            elif actions[i-1].price < j:
+                if (actions[i-1].profitability + mat[i-1][j-actions[i-1].price].value) > mat[i-1][j].value:
+                    mat[i][j].add_action(actions[i-1])
+                    mat[i][j].add_action(mat[i-1][j - actions[i-1].price].action_list)
+                else:
+                    mat[i][j] = mat[i-1][j]
+            else:
+                mat[i][j] = mat[i-1][j]
+
     return mat[n][W]
